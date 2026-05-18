a09229f6d7395f9f417277087b354b6e3bcbc71a
frederic76430 <79060970+frederic76430@users.noreply.github.com>
2026-05-18 12:27:56 +0200

    Add files via upload

---
+++ custom_components/micro_onduleur_hypontech/entity.py
@@ -0,0 +1,34 @@
+"""Entité de base pour Micro Onduleur Hypontech."""
+
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .const import DOMAIN
+from .coordinator import MicroOnduleurHypontechCoordinator
+
+
+class MicroOnduleurEntity(CoordinatorEntity[MicroOnduleurHypontechCoordinator]):
+    """Entité de base pour Micro Onduleur Hypontech."""
+
+    _attr_has_entity_name = True
+
+    def __init__(
+        self,
+        coordinator: MicroOnduleurHypontechCoordinator,
+        entry_id: str,
+        entry_title: str,
+    ) -> None:
+        """Initialiser l'entité."""
+        super().__init__(coordinator)
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, entry_id)},
+            name=entry_title,
+            manufacturer="Hypontech",
+            model="HMS-800W-C",
+            configuration_url="https://www.hypon.cloud",
+        )
+
+    @property
+    def available(self) -> bool:
+        """Retourner si l'entité est disponible."""
+        return super().available and self.coordinator.data is not None
